4eea5b472eb07e8982cca955f87638402a074175
Monty <monty@mariadb.org>
2016-12-20 13:03:45 +0200

    Removed usage of my_hash_search() with uninitialized HASH. - Not documented on intened usage - Extra checking takes time for all HASH usage

--- storage/innobase/include/srv0srv.h
+++ storage/innobase/include/srv0srv.h
@@ -288,9 +288,7 @@ extern my_bool	srv_numa_interleave;
 extern my_bool srv_use_trim;
 
 /* Use posix fallocate */
-#ifdef HAVE_POSIX_FALLOCATE
 extern my_bool srv_use_posix_fallocate;
-#endif
 
 /* Use atomic writes i.e disable doublewrite buffer */
 extern my_bool srv_use_atomic_writes;
